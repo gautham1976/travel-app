248cef7009f7587f02dc8d04dc06985a102610a7
gautham1976 <gautham1976@gmail.com>
2022-07-08 02:22:09 +0530

    Project Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TravelRegistrationComponent } from './travel-registration/travel-registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelRegistrationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/travel-registration/travel-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travel-registration',
+  templateUrl: './travel-registration.component.html',
+  styleUrls: ['./travel-registration.component.css']
+})
+export class TravelRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

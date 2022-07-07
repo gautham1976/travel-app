666c31c3bd17b20262557ed35ef82425c1ce9117
gautham1976 <gautham1976@gmail.com>
2022-07-08 02:42:15 +0530

    View Destination completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TravelRegistrationComponent } from './travel-registration/travel-registration.component';
+import { ViewDestinationsComponent } from './view-destinations/view-destinations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TravelRegistrationComponent
+    TravelRegistrationComponent,
+    ViewDestinationsComponent
   ],
   imports: [
     BrowserModule,
